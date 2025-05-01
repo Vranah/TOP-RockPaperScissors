871f24553852f6fbd07b3a22a6f17d752c314652
vranah <dilyantsekov@gmail.com>
2025-05-01 14:58:12 +0300

    Fix the comparison logic and change the rng function
    Switc the choices and inputs to non case sensitive
    strings.The comparisons function will now compare
    corrctly and the rnadom nuber generator uses floor
    instead of ciel

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,33 +1,44 @@
 function getComputerChoice() {
-    let guess = Math.ceil(Math.random() * 3);
-    return guess;
+    let guess = Math.floor(Math.random() * 3 +1);
+    if(guess === 1) {
+        return `rock`;
+    }else if(guess === 2) {
+        return `paper`;
+    }else if(guess === 3) {
+        return `scissors`;
+    }
 }
 
 function getPlayerChoice() {
     let guess = prompt(`Make a choice: <ROCK> <PAPER> <SCISSORS>`)
         if(guess.toLowerCase() === `rock`){
-            return 1;
+            return `rock`;
         }else if (guess.toLowerCase() === `paper`) {
-            return 2;
+            return `paper`;
         }else if (guess.toLowerCase() === `scissors`) {
-            return 3;
+            return `scissors`;
         }
 }
 
 function playRound(computerChoice, humanChoice) {
     
-    if(computerChoice === humanChoice){
-        draw++;
-        console.log(`It's a draw!`);
-        
-    }else if(computerChoice > humanChoice) {
-        humanScore++;
-        console.log(`YOU WON! The score is: Player: ${humanScore} Computer: ${computerScore}`);
-        
-    }else if(computerChoice < humanChoice){
+    if((computerChoice ===`rock` && humanChoice === `scissors`) ||
+       (computerChoice ===`paper` && humanChoice === `rock`) ||
+       (computerChoice ===`scissors` && humanChoice === `paper`)) {
         computerScore++;
-        console.log(`YOU LOST! The score is: Player: ${humanScore} Computer: ${computerScore}`);
+        console.log(` ${computerChoice} vs ${humanChoice} YOU LOOSE!`);
+
+    } else if((computerChoice ===`paper` && humanChoice === `scissors`) ||
+              (computerChoice ===`rock` && humanChoice === `paper`) ||
+              (computerChoice ===`scissors` && humanChoice === `rock`)) {
+                humanScore++;
+                console.log(` ${computerChoice} vs ${humanChoice} YOU WIN!`);
+    }else if(computerChoice === humanChoice) {
+        draw++;
+        console.log(` ${computerChoice} vs ${humanChoice} DRAW!`);
     }
+        
+    
     
 }
 
@@ -35,17 +46,22 @@ let humanScore = 0;
 let computerScore = 0;
 let draw = 0;
 
-for(let i = 0; i <= 5; i++) {
+
+function playGame() {
+    for(let i = 0; i <= 5; ++i) {
     if(i === 5 && humanScore > computerScore){
         console.log(`YOU WON!
-            Final score: Player: ${humanScore} Computer: ${computerScore} Draw: ${draw}`);
+            Final score: Player: ${humanScore} Computer: ${computerScore} Draws: ${draw}`);
          }else if (i === 5 && humanScore > computerScore){
         console.log(`YOU LOST!
-                Final score: Player: ${humanScore} Computer: ${computerScore} Draw: ${draw}`);
-         }else if (i === 5 && humanScore == computerScore){
+                Final score: Player: ${humanScore} Computer: ${computerScore} Draws: ${draw}`);
+         }else if (i === 5 && humanScore === computerScore){
             console.log(`DRAW!
-                    Final score: Player: ${humanScore} Computer: ${computerScore} Draw: ${draw}`);
+                    Final score: Player: ${humanScore} Computer: ${computerScore} Draws: ${draw}`);
              }else{
             playRound(getComputerChoice(), getPlayerChoice());
-         }
+            }
+        }
 }
+
+playGame()
