5c9cd0bd6c8a66c8bd8c5db285f5137fdd1aba17
vranah <dilyantsekov@gmail.com>
2025-06-26 18:06:59 +0300

    Save branch changes

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,19 @@
+
+const btns = document.querySelectorAll(`button`);
+const round = document.querySelector(`.round`);
+const roundInfo = document.querySelector(`.roundInfo`);
+const score = document.querySelector(`.score`);
+
+
+
+
+btns.forEach(btn => btn.addEventListener('click', event => {
+    playGame();
+    })
+);
+
+
+
 function getComputerChoice() {
     let guess = Math.floor(Math.random() * 3 +1);
     if(guess === 1) {
@@ -9,59 +25,65 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let guess = prompt(`Make a choice: <ROCK> <PAPER> <SCISSORS>`)
-        if(guess.toLowerCase() === `rock`){
-            return `rock`;
-        }else if (guess.toLowerCase() === `paper`) {
-            return `paper`;
-        }else if (guess.toLowerCase() === `scissors`) {
-            return `scissors`;
-        }
+function getPlayerChoice(event) {
+    let guess = event.target.textContent.toLowerCase();
+    return guess;
+        
 }
+
+// function getPlayerChoice() {
+//     let guess = prompt(`Make a choice: <ROCK> <PAPER> <SCISSORS>`)
+//         if(guess.toLowerCase() === `rock`){
+//             return `rock`;
+//         }else if (guess.toLowerCase() === `paper`) {
+//             return `paper`;
+//         }else if (guess.toLowerCase() === `scissors`) {
+//             return `scissors`;
+//         }
+// }
 
 function playRound(computerChoice, humanChoice) {
     
     if((computerChoice ===`rock` && humanChoice === `scissors`) 
        || (computerChoice ===`paper` && humanChoice === `rock`) 
        || (computerChoice ===`scissors` && humanChoice === `paper`)) {
-        computerScore++;
-        console.log(` ${computerChoice} vs ${humanChoice} YOU LOOSE!`);
+        ++rounds;
+        computerScore += 1;
+        roundInfo.textContent = `${computerChoice} vs ${humanChoice} - YOU LOOSE!`;
 
     } else if((computerChoice ===`paper` && humanChoice === `scissors`) 
               || (computerChoice ===`rock` && humanChoice === `paper`) 
               || (computerChoice ===`scissors` && humanChoice === `rock`)) {
-                humanScore++;
-                console.log(` ${computerChoice} vs ${humanChoice} YOU WIN!`);
+                ++rounds ;
+                humanScore += 1;        
+                roundInfo.textContent = `${computerChoice} vs ${humanChoice} - YOU WIN!`
     }else if(computerChoice === humanChoice) {
-        draw++;
-        console.log(` ${computerChoice} vs ${humanChoice} DRAW!`);
+        ++rounds ;
+        draw += 1;
+        roundInfo.textContent =  `${computerChoice} vs ${humanChoice} - DRAW!`;
     }
-        
-    
-    
+    round.textContent = `Round: ${rounds}`;
+    score.textContent = `PC: ${computerScore} Player:${humanScore} Draws:${draw}`
 }
 
 let humanScore = 0;
 let computerScore = 0;
 let draw = 0;
+let rounds = 0;
 
 
 function playGame() {
-    for(let i = 0; i <= 5; ++i) {
-    if(i === 5 && humanScore > computerScore){
-        console.log(`YOU WON!
-            Final score: Player: ${humanScore} Computer: ${computerScore} Draws: ${draw}`);
-         }else if (i === 5 && humanScore > computerScore){
-        console.log(`YOU LOST!
-                Final score: Player: ${humanScore} Computer: ${computerScore} Draws: ${draw}`);
-         }else if (i === 5 && humanScore === computerScore){
-            console.log(`DRAW!
-                    Final score: Player: ${humanScore} Computer: ${computerScore} Draws: ${draw}`);
-             }else{
-            playRound(getComputerChoice(), getPlayerChoice());
+    
+    if(rounds === 5 && humanScore > computerScore){
+        roundInfo.textContent =`YOU WON!`;
+         }else if (rounds === 5 && computerScore > humanScore){
+        roundInfo.textContent = `YOU LOST!`;
+         }else if (rounds === 5){
+            roundInfo.textContent = `DRAW!`;
+             }else{getComputerChoice;
+            playRound(getComputerChoice(), getPlayerChoice(event));
             }
-        }
+        
 }
 
-playGame()
+//playGame()
